ff03cb03f088e0123781e13dd327cab5b0b9c5eb
Adam Ren <adam_ren@sina.com>
2020-02-17 23:12:12 +0800

    init my first step to program in Python

---
+++ helloworld/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# @Time: 2020-02-17 23:10
+# @Project: python-basic-knowledge
+# @Version: 2020.02 builder 172310
+# @Author: Adam Ren
+# @Email: adam_ren@sina.com
+# @Github: https://github.com/ren-adam
+# @Site: http://renpeter.com
+# @File : __init__.py.py
+# @Software: PyCharm
+#
+
+
---
+++ helloworld/hello_world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# @Time: 2020-02-17 23:10
+# @Project: python-basic-knowledge
+# @Version: 2020.02 builder 172310
+# @Author: Adam Ren
+# @Email: adam_ren@sina.com
+# @Github: https://github.com/ren-adam
+# @Site: http://renpeter.com
+# @File : hello_world.py
+# @Software: PyCharm
+#
+
+
+"""
+This is my first program in Python to say Hello, World to the new world.
+"""
+
+
+print('Hello, World!')
+print('世界，你好！')
